b7aec72b460e9548265eb0b758a4fd9f0631bcaa
georgeWasfy <g.a.wasfy@gmail.com>
2024-06-22 23:23:46 +0300

    cleanup unused functions

--- src/lib/common/http/httpRoute.ts
+++ src/lib/common/http/httpRoute.ts
@@ -7,36 +7,6 @@ export class HttpRoute {
     public name: string,
     public url: string,
     public method: HttpMethodEnum,
-    public params?: {type: string, idx: number}[]
+    public params?: { type: string; idx: number }[]
   ) {}
-
-  public static getBaseUrl(target: Object): string {
-    return Reflect.getOwnMetadata("custom:baseUrl", target);
-  }
-
-  public static buildUrl(
-    target: Object,
-    url: string,
-    root?: string
-  ): string {
-    let rootUrl = root ? this.nomalizeUrl(root) : null;
-    let baseUrl =
-      this.nomalizeUrl(
-        rootUrl
-          ? rootUrl + "/" + this.getBaseUrl(target)
-          : this.getBaseUrl(target)
-      ) || "/";
-    let returnUrl =
-      baseUrl.length === 1 && url
-        ? this.nomalizeUrl(url)
-        : baseUrl + this.nomalizeUrl(url);
-
-    return returnUrl;
-  }
-
-  private static nomalizeUrl(url: string): string {
-    if (url) return url.startsWith("/") ? url : "/" + url;
-
-    return "";
-  }
 }
